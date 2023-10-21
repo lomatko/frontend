8f59441020f40ead1739b58f3494d924c76f19c0
Roksana <roksana.traczyk@wp.pl>
2023-10-21 15:28:44 +0200

    Added functionality to add reward

--- collabothon2023/src/component/EventsComponent.js
+++ collabothon2023/src/component/EventsComponent.js
@@ -1,30 +1,72 @@
 import {CardComponent} from "./CardComponent";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Grid from '@mui/material/Grid';
+import axios from "axios";
 
 export function EventsComponent(props) {
+    const[events, setEvents] = useState([]);
 
     // TODO: call to backend for list of events
     // --ALL EVENTS
     // -- USER EVENTS
+
+
+    useEffect(() => {
+        axios
+            .get("https://backend-3u6yq4mi5q-ew.a.run.app/events")
+            .then((response) => {
+                console.log('response', response)
+                setEvents(response.data)
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    const convertDate = (event) => {
+        return formattedDate(event.startDate) + ' '
+            + getFormattedTime(event.startDate) + '-'
+            + getFormattedTime(event.endDate);
+    }
+
+    function formattedDate(date) {
+        let d = new Date(date)
+        let month = String(d.getMonth() + 1);
+        let day = String(d.getDate());
+        const year = String(d.getFullYear());
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return `${day}/${month}/${year}`;
+    }
+
+    function getFormattedTime(date) {
+        let d = new Date(date);
+        let hour = String(d.getHours())
+        let minutes = String(d.getMinutes())
+
+        if (hour.length < 2) hour = '0' + hour;
+        if (minutes.length < 2) minutes = '0' + minutes;
+
+        return `${hour}:${minutes}`;
+    }
 
     return (
 
         <div>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 {
-                    props.cards.map(card => {
+                    events.map(event => {
                         return <Grid item xs={2}>
                             <CardComponent
-                            title={card.title}
-                            coins={card.coins}
-                            description={card.description}
-                            participants={card.participants}
-                            time={card.time}
+                            title={event.title}
+                            coins={event.coins}
+                            description={event.description}
+                            participants={event.participants}
+                            time={convertDate(event)}
                             buttonMessage={props.buttonMessage}
-                            isFinished={card.isFinished}
+                            isFinished={event.isFinished}
                             buttonHide={props.buttonHide}
-                            location={card.location}/>
+                            location={event.location}/>
                         </Grid>
                     })
                 }
--- collabothon2023/src/component/RewardsComponent.js
+++ collabothon2023/src/component/RewardsComponent.js
@@ -1,32 +1,56 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import '../style/login-styles.scss'
 import Button from "@mui/material/Button";
 import '../style/card-style.scss';
 import Grid from "@mui/material/Grid";
 import {CardComponent} from "./CardComponent";
 import {RewardCardComponent} from "./RewardCardComponent";
+import axios from "axios";
+import {AddRewardCardComponent} from "./AddRewardCardComponent";
 
 
 export function RewardsComponent(props) {
+    const [rewards, setRewards] = useState([]);
+    const [refreshCounter, setRefreshCounter] = useState(0);
+
+    useEffect(() => {
+        axios
+            .get("https://backend-3u6yq4mi5q-ew.a.run.app/rewards")
+            .then((response) => {
+                setRewards(response.data)
+            })
+            .catch((err) => console.log(err));
+    }, [refreshCounter]);
+
 
     const AVAILABLE_COINS = 9;
+
+    const refreshData = () => {
+        setRefreshCounter(refreshCounter + 1);
+    }
 
     return (
         <div>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 {
-                    props.rewards.map(card => {
+                    rewards.map(reward => {
                         return <Grid item xs={2}>
                             <RewardCardComponent
-                                title={card.title}
-                                coins={card.coins}
+                                title={reward.name}
+                                coins={reward.price}
                                 isBank={props.isBank}
                                 userCoins={props.isBank? undefined : AVAILABLE_COINS}
-                                description={card.description}
+                                description={reward.description}
                                 buttonMessage={props.buttonMessage}/>
                         </Grid>
                     })
                 }
+                {
+                    props.isBank &&  <Grid item xs={2}>
+                        <AddRewardCardComponent callBack={() => refreshData()}/>
+                    </Grid>
+                }
+
             </Grid>
         </div>
     );
